118da643444090ca99fec5c8e43a592837de1a0a
cristusrexus <cristianmihaiandrei3@gmail.com>
2024-08-30 15:24:09 +0300

    Created Reservation.java

---
+++ src/main/java/sda/academy/entities/Reservation.java
@@ -0,0 +1,88 @@
+package sda.academy.entities;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "reservation_date")
+    private Date reservationDate;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+
+    public Reservation() {}
+
+    public Reservation(Date reservationDate, Date startDate, Date endDate, Customer customer, Car car) {
+        this.reservationDate = reservationDate;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.customer = customer;
+        this.car = car;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
