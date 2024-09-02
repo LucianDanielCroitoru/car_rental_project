7f769127a51852bb293afd2a0c5366806495ba20
LucianDanielCroitoru <73315585+LucianDanielCroitoru@users.noreply.github.com>
2024-09-02 18:27:35 +0300

    Update Reservation.java
    Reviewed Reservation.java

--- src/main/java/sda/academy/entities/Reservation.java
+++ src/main/java/sda/academy/entities/Reservation.java
@@ -4,11 +4,13 @@ import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
+@Getter
+@Setter
 public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
 
     @Column(name = "reservation_date")
     private Date reservationDate;
@@ -26,64 +28,4 @@ public class Reservation {
     @ManyToOne
     @JoinColumn(name = "car_id")
     private Car car;
-
-
-    public Reservation() {}
-
-    public Reservation(Date reservationDate, Date startDate, Date endDate, Customer customer, Car car) {
-        this.reservationDate = reservationDate;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.customer = customer;
-        this.car = car;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getReservationDate() {
-        return reservationDate;
-    }
-
-    public void setReservationDate(Date reservationDate) {
-        this.reservationDate = reservationDate;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public Car getCar() {
-        return car;
-    }
-
-    public void setCar(Car car) {
-        this.car = car;
-    }
 }
